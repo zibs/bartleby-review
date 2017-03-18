9e98025b570e4f907c260d3cac7638e1500c8838
zibs <elizibin@gmail.com>
2017-03-18 11:18:25 -0700

    optimize images and add media queries

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
 $(document).ready(function() {
-  var MAX_HEIGHT = 150
-  var MINIMUM_HEIGHT = 75
 
-  $("img").each(function(index) {
-    var getHeight = function () {
-      return Math.floor((Math.random() * MAX_HEIGHT) + MINIMUM_HEIGHT);
-    }
+  var shuffleBartlebys = function () {
+    var MAX_HEIGHT = 250
+    var MINIMUM_HEIGHT = 75
 
-    var getPosition = function () {
-      var possibleWidth = $(".bartlebys").width() - 200
-      var possibleHeight = $(".bartlebys").height() - 200
-      var width = Math.floor((Math.random() * possibleWidth) + MINIMUM_HEIGHT);
-      var height = Math.floor((Math.random() * possibleHeight) + MINIMUM_HEIGHT);
+    $("img").each(function(index) {
+      var getHeight = function () {
+        return Math.floor((Math.random() * MAX_HEIGHT) + MINIMUM_HEIGHT);
+      }
 
-      return {width: width, height: height}
-    }
-    var height = getHeight()
-    var position = getPosition()
-    $(this).css({'max-height': height + 'px', left: position.width + 'px', position: 'absolute', top: position.height + "px"})
-  });
+      var getPosition = function () {
+        var possibleWidth = $(".bartlebys").width() - 200
+        var possibleHeight = $(".bartlebys").height() - 200
+        var width = Math.floor((Math.random() * possibleWidth) + MINIMUM_HEIGHT);
+        var height = Math.floor((Math.random() * possibleHeight) + MINIMUM_HEIGHT);
+
+        return {width: width, height: height}
+      }
+      var height = getHeight()
+      var position = getPosition()
+      $(this).css({'max-height': height + 'px', left: position.width + 'px', position: 'absolute', top: position.height + "px"})
+    });
+  }
+
+  shuffleBartlebys()
+  $('.site-title').click(function() {
+      shuffleBartlebys()
+  })
 });
+
 
 $(document).ready(function() {
   $( ".draggable" ).draggable({
