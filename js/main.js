2b22cf7349f35860de29ffc8813870a9bc2082f8
zibs <elizibin@gmail.com>
2017-03-18 18:54:12 -0700

    refactor js into functions

--- js/main.js
+++ js/main.js
@@ -4,31 +4,59 @@ $(document).ready(function() {
     var MAX_HEIGHT = 250
     var MINIMUM_HEIGHT = 75
 
+    // var COLORS = ['#FD97A5', '#FEFEC0', '#9DF8FD', '#BBFED5', '#FB4A64', '#D5B79D', '#E3E4E8', '#FBED67', '#FBC9FC', ]
+    // var color = COLORS[Math.floor((Math.random() * COLORS.length))]
+    // $(".bartlebys").css({'border-left-color': color})
+
     $("img").each(function(index) {
       var getHeight = function () {
         return Math.floor((Math.random() * MAX_HEIGHT) + MINIMUM_HEIGHT);
       }
 
       var getPosition = function () {
-        var possibleWidth = $(".bartlebys").width() - 200
-        var possibleHeight = $(".bartlebys").height() - 200
-        var width = Math.floor((Math.random() * possibleWidth) + MINIMUM_HEIGHT);
+        var possibleWidth = $(".bartlebys").outerWidth() - 300
+        var possibleHeight = $(".bartlebys").height() - 350
+        var left = Math.floor((Math.random() * possibleWidth) + MINIMUM_HEIGHT);
         var height = Math.floor((Math.random() * possibleHeight) + MINIMUM_HEIGHT);
 
-        return {width: width, height: height}
+        return {left: left, height: height}
       }
       var height = getHeight()
       var position = getPosition()
-      $(this).css({'max-height': height + 'px', left: position.width + 'px', position: 'absolute', top: position.height + "px", 'opacity': 1})
 
+      $(this).css({'max-height': height + 'px', left: position.left + 'px', position: 'absolute', top: position.height + "px", opacity: 1})
+      $(".bartlebys").css({height: 100 + 'vh'})
+      $(".scrollable").css({'max-height': 100 + 'vh'})
     });
   }
 
-  shuffleBartlebys()
+  var organizeBartlebys = function() {
+    $("img.review").each(function(index) {
+      $(this).css({'max-height': 150 + 'px', position: 'relative', left: 0, top: 0})
+    });
+    $(".bartlebys").css({height: 'inherit'})
+    $(".scrollable").css({'max-height': 'inherit'})
+  }
+
+
 
   $('.site-title').click(function() {
       shuffleBartlebys()
   })
+
+  $('#site-map').click(function() {
+    organizeBartlebys()
+  })
+
+  $(window).on('resize', function(event){
+    var windowWidth = $(window).width();
+    if (windowWidth < 568) {
+      organizeBartlebys()
+    }
+  });
+
+  shuffleBartlebys()
+
 });
 
 
